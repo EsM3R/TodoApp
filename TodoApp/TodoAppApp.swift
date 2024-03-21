22ed51dcf3bfc18c229a40e60794f17988ab6a67
Mehmet Vural <105605335+EsM3R@users.noreply.github.com>
2024-03-21 22:17:04 +0300

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Mehmet Vural on 21.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
